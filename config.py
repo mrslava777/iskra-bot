fd99aec87bcb7cd4bc5227dcbcbea9a55083e47d
mrslava777 <slawa226834319@gmail.com>
2026-06-28 22:48:30 +0300

    fix(config): remove patch markers leftover

--- config.py
+++ config.py
@@ -2,6 +2,12 @@
 *** Update File: config.py
 @@
 -DATABASE_URL = os.getenv("DATABASE_URL", "")
+-PGBOUNCER_URL = os.getenv("PGBOUNCER_URL", "")
+-DB_POOL_MIN = int(os.getenv("DB_POOL_MIN", "2"))
+-DB_POOL_MAX = int(os.getenv("DB_POOL_MAX", "10"))
+-
+-# Redis
+-REDIS_URL = os.getenv("REDIS_URL", "")
 +DATABASE_URL = os.getenv("DATABASE_URL", "")
 +PGBOUNCER_URL = os.getenv("PGBOUNCER_URL", "")
 +DB_POOL_MIN = int(os.getenv("DB_POOL_MIN", "2"))
@@ -10,9 +16,17 @@
 +# Redis
 +REDIS_URL = os.getenv("REDIS_URL", "")
 @@
- MAX_TRACKED_USERS = int(os.getenv("MAX_TRACKED_USERS", "10000"))
+-MAX_TRACKED_USERS = int(os.getenv("MAX_TRACKED_USERS", "10000"))
 @@
- SENTRY_DSN = os.getenv("SENTRY_DSN", "")
+-SENTRY_DSN = os.getenv("SENTRY_DSN", "")
+-
+-# Send queue / workers
+-SEND_CONCURRENCY = int(os.getenv("SEND_CONCURRENCY", "20"))
+-NUM_WORKERS = int(os.getenv("NUM_WORKERS", "4"))
+-MAX_MESSAGE_RATE_GLOBAL = int(os.getenv("MAX_MESSAGE_RATE_GLOBAL", "20"))
++MAX_TRACKED_USERS = int(os.getenv("MAX_TRACKED_USERS", "10000"))
+@@
++SENTRY_DSN = os.getenv("SENTRY_DSN", "")
 +
 +# Send queue / workers
 +SEND_CONCURRENCY = int(os.getenv("SEND_CONCURRENCY", "20"))
